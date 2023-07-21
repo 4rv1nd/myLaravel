4af1fa3491e5a13e080599c02de0840c1576fed9
= <=>
2023-07-21 21:49:17 +0530

    added mutations and casts

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -18,4 +19,24 @@ class Post extends Model
     // public function commnent(){
     //     return $this->hasManyThrough(Post::class);
     // }
+
+    // before laravel 9
+    // public function getTitleAttribute($title){
+    //     return strtoupper($title);
+    // }
+    // public function setTitleAttribute($value){
+    //     return $this->attributes['title'] = strtoupper($value);
+    // }
+
+    // after laravel 9
+    // protected function title(): Attribute {
+    //     return Attribute::make(
+    //         get: fn ($value) => strtoupper($value),
+    //         set: fn ($value) => strtoupper($value)
+    //     );
+    // }
+
+    protected $casts = [
+        'description'=>'encrypted'
+    ];
 }
